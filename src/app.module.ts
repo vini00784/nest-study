1735090f62bfc6d4cea9ba41a0149b9b727414b0
vini00784 <vo70346@gmail.com>
2024-06-24 16:00:39 -0300

    feat: setting config module to access env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       playground: true,
       driver: ApolloDriver
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
     })
   ],
   controllers: [],
